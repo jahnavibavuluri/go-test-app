8caab1fe02c06bea4b99d1b5e4b48fb0b8c96781
jbavulur <jbavulur@mathworks.com>
2025-01-14 10:45:18 -0500

    created demo app

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+var toolboxes = []string{
+    "Signal Processing Toolbox",
+    "Optimization Toolbox",
+    "Deep Learning Toolbox",
+    "Computer Vision Toolbox",
+}
+
+func main() {
+    http.HandleFunc("/", toolboxHandler)
+    fmt.Println("Server started at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
+
+func toolboxHandler(w http.ResponseWriter, r *http.Request) {
+    html := "<html><head><title>MATLAB Toolboxes</title></head><body>"
+    html += "<h1>List of MATLAB Toolboxes</h1><ul>"
+
+    for _, toolbox := range toolboxes {
+	html += fmt.Sprintf("<li>%s</li>", toolbox)
+    }
+
+    html += "</ul></body></html>"
+    w.Header().Set("Content-Type", "text/html")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(html))
+}
